2f86494c1dbb0178352115fe201fd2d57bd851dd
Karim Naguib <karimn2.0@gmail.com>
2021-01-12 11:35:00 -0500

    Add na.rm option

--- multilvlr_util.R
+++ multilvlr_util.R
@@ -7,10 +7,10 @@ quantiles_pivot_wider <- function(quantiles_data) {
   pivot_wider(quantiles_data, names_from = per, values_from = est, names_prefix = "per_")
 }
 
-quantilize_est <- function(iter, var, wide = TRUE, quant_probs = c(0.05, 0.1, 0.5, 0.9, 0.95)) {
+quantilize_est <- function(iter, var, wide = TRUE, quant_probs = c(0.05, 0.1, 0.5, 0.9, 0.95), na.rm = FALSE) {
   quant_data <- iter %>% 
     pull({{ var }}) %>% 
-    quantile(probs = quant_probs, names = FALSE) %>% 
+    quantile(probs = quant_probs, names = FALSE, na.rm = na.rm) %>% 
     enframe(name = NULL, value = "est") %>% 
     mutate(per = quant_probs)
   
